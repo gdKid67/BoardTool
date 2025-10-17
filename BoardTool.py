4d3cbf44629a7aa43994715272b09b8039eb0bb4
4Micro <gdKid67@gmail.com>
2025-10-17 18:36:16 +0900

    Update BoardTool.py
    Added a file button so only one window appears on popup.

--- BoardTool.py
+++ BoardTool.py
@@ -5,32 +5,32 @@ print("Contact my email(gdKid67@gmail.com)")
 
 import tkinter as tk
 from tkinter import filedialog
+def file_open():
+    def export():
+        types = [("KiCAD PCB", "*.kicad_pcb"),
+             ("All files", "*.*")]
+        file = filedialog.asksaveasfile(title = "Export",
+                                    initialdir = ".",
+                                    defaultextension=".kicad_pcb",
+                                    filetypes=types)
+    
+    root = tk.Tk()
+    root.title("file")
+    export = tk.Button(root, text="Export", command=export)
+    export.pack()
+
+    import_BT_Function = tk.Button(root, text="Import")
+    import_BT_Function.pack()
+
 
 
 root = tk.Tk()
 root.title("BoardTool 0.0.1")
 root.minsize(500, 500) #(Width, Height)
+file = tk.Button(root, text="File", command=file_open)
+file.pack()
 
-print("Opened BoardTool[ 1/2 windows ]")
-
-root = tk.Tk()
-root.title("Controls")
-
-def export():
-    types = [("KiCAD PCB", "*.kicad_pcb"),
-             ("All files", "*.*")]
-    file = filedialog.asksaveasfile(title = "Export",
-                                    initialdir = ".",
-                                    defaultextension=".kicad_pcb",
-                                    filetypes=types)
-
-export = tk.Button(root, text="Export", command=export)
-export.pack()
-
-import_BT_Function = tk.Button(root, text="Import")
-import_BT_Function.pack()
-
-print("Opened BoardTool[ 2/2 windows]")
+print("Opened BoardTool")
 
 root.mainloop()
 print("Closed BoardTool")
